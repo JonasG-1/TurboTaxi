c4366ca2502f29bb3f2e9385cf40154b660a93e0
JonasG-1 <jonas.gold_@outlook.de>
2023-02-15 08:45:12 +0100

    Benutzerverwaltung wurde hinzugefügt und Verbindung angepasst.

--- src/Server/Benutzerverwaltung.java
+++ src/Server/Benutzerverwaltung.java
@@ -1,5 +1,60 @@
 package Server;
 
+import linearestrukturen.List;
+
 public class Benutzerverwaltung {
 
+    private List<Verbindung> hatVerbindungen;
+    private int hatAnzahlVerbindungen;
+
+    public Benutzerverwaltung() {
+        hatVerbindungen = new List<Verbindung>();
+        hatAnzahlVerbindungen = 0;
+    }
+
+    public void verbindungHinzufuegen(Verbindung pVerbindung) {
+        hatVerbindungen.append(pVerbindung);
+        hatAnzahlVerbindungen++;
+    }
+
+    public void verbindungEntfernen(Verbindung pVerbindung) {
+        hatVerbindungen.toFirst();
+        boolean gefunden = false;
+        while(hatVerbindungen.hasAccess() && !gefunden) {
+            if (hatVerbindungen.getContent().equals(pVerbindung)) {
+                hatVerbindungen.remove();
+                hatAnzahlVerbindungen--;
+                gefunden = true;
+            }
+            hatVerbindungen.next();
+        }
+    }
+
+    public int gibAnzahlVerbindungen() {
+        return hatAnzahlVerbindungen;
+    }
+
+    public Verbindung gibBenutzer(String pName) {
+        hatVerbindungen.toFirst();
+        Verbindung verbindung = null;
+        while (hatVerbindungen.hasAccess()) {
+            if (hatVerbindungen.getContent().gibName().equals(pName)) {
+                verbindung = hatVerbindungen.getContent();
+            }
+            hatVerbindungen.next();
+        }
+        return verbindung;
+    }
+
+    public Verbindung gibBenutzer(String pIP, int pPort) {
+        hatVerbindungen.toFirst();
+        Verbindung verbindung = null;
+        while (hatVerbindungen.hasAccess()) {
+            if (hatVerbindungen.getContent().equals(new Verbindung(pIP, pPort))) {
+                verbindung = hatVerbindungen.getContent();
+            }
+            hatVerbindungen.next();
+        }
+        return verbindung;
+    }
 }
--- src/Server/Verbindung.java
+++ src/Server/Verbindung.java
@@ -35,4 +35,14 @@ public class Verbindung {
     public void setzeFarbe(Farbe zFarbe) {
         this.zFarbe = zFarbe;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean gleich = false;
+        if (obj instanceof Verbindung) {
+           Verbindung verbindung = (Verbindung) obj;
+           gleich = verbindung.gibIP().equals(zIP) && verbindung.gibPort() == zPort;
+        }
+        return gleich;
+    }
 }
